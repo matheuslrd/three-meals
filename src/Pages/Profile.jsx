ded7c7a5c9df06a195ddd6fe3e6b138b30d165f5
Matheus Laurindo <matheus104521@gmail.com>
2021-11-17 17:10:13 -0300

    Conclui requisito 82

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
+import '../Styles/Profile.css';
+
+import Button from '../Components/Button';
+
 function Profile() {
   return (
     <main className="Profile">
@@ -11,6 +15,36 @@ function Profile() {
       >
         Perfil
       </Header>
+      <article className="main-content">
+        <section data-testid="profile-email">
+          emailDaPessoa@email.com
+        </section>
+        <section className="container-links">
+          <Button
+            className="profile-done-btn btns-links-profile"
+            dataTestId="profile-done-btn"
+            hasLink="/receitas-feitas"
+          >
+            Recipes Done
+          </Button>
+
+          <Button
+            className="profile-favorite-btn btns-links-profile"
+            dataTestId="profile-favorite-btn"
+            hasLink="/receitas-favoritas"
+          >
+            Favorite Recipes
+          </Button>
+
+          <Button
+            className="profile-favorite-btn btns-links-profile"
+            dataTestId="profile-logout-btn"
+            hasLink="/"
+          >
+            Logout
+          </Button>
+        </section>
+      </article>
       <Footer />
     </main>
   );
