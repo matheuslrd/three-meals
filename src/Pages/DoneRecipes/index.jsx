91e5d9e9581a3c83841133c04001ad22bb9d4b0e
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-11-19 19:01:12 -0300

    Branch mergeada

--- src/Pages/DoneRecipes/index.jsx
+++ src/Pages/DoneRecipes/index.jsx
@@ -1,4 +1,4 @@
-/* import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Header from '../../Components/Header';
 import ButtonsFilter from '../../Components/ButtonsFilter';
@@ -39,4 +39,3 @@ function RecipesMade() {
 }
 
 export default RecipesMade;
- */
